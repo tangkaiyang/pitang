ae1e5b8045d3abbda96143b70524bc6f2b916a99
tangky <945541696@qq.com>
2022-09-26 15:13:43 +0800

    跨域

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 
 pitang = Flask(__name__)
-
+CORS(pitang, supports_credentials=True)
 
 pitang.config.from_object(Config)
