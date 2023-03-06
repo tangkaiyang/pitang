ef75a0f813231abf07740cd59841f68858d19e8a
tangky <945541696@qq.com>
2023-03-06 15:08:29 +0800

    注册test_case

--- run.py
+++ run.py
@@ -3,12 +3,14 @@ from app.utils.logger import Log
 from app.controllers.auth.user import auth
 from app.controllers.request.http import req
 from app.controllers.project.project import pr
+from app.controllers.testcase.testcase import ts
 from app import dao
 
 # 注册蓝图
 pitang.register_blueprint(auth)
 pitang.register_blueprint(req)
 pitang.register_blueprint(pr)
+pitang.register_blueprint(ts)
 
 
 @pitang.route('/')
