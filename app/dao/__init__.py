0e68cb32184a8f0a08fedf3de1d2c6f13338f891
tangky <945541696@qq.com>
2023-03-22 19:59:32 +0800

    global_config model

--- app/dao/__init__.py
+++ app/dao/__init__.py
@@ -4,6 +4,7 @@ from app.models.test_case import TestCase
 from app.models.project import Project
 from app.models.project_role import ProjectRole
 from app.models.environment import Environment
+from app.models.global_config import GlobalConfig
 
 db.create_all()
 
