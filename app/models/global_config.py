2823623c460ce8ab7a490e7f37501df928e7d3e8
大胖子 <945541696@qq.com>
2023-03-23 07:36:59 +0800

    global_config

---
+++ app/models/global_config.py
@@ -0,0 +1,25 @@
+from app.models import db
+from app.models.base_model import BaseModel
+from sqlalchemy import UniqueConstraint
+
+
+class GlobalConfig(BaseModel):
+    id = db.Column(db.INT, primary_key=True)
+    env = db.Column(db.INT)
+    key = db.Column(db.String(16))
+    value = db.Column(db.Text)
+    key_type = db.Column(db.INT, nullable=False,
+                         comment="0:string 1: json 2:yaml")
+    enable = db.Column(db.BOOLEAN, default=True)
+
+    __table_args__ = (UniqueConstraint('env', 'key', 'deleted_at'),)
+
+    def __init__(self, env, key, value, key_type, enable,  user, id=0):
+        super().__init__(user)
+
+        self.id = id
+        self.env = env
+        self.key = key
+        self.value = value
+        self.key_type = key_type
+        self.enable = enable
---
+++ app/models/schema/global_config.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, validator
+from app.exceptions.ParamsException import ParamsError
+
+
+class GlobalConfig(BaseModel):
+    id: int = None
+    key: str
+    value: str
+    env: str = None
+    key_type: int
+    enable: bool
+
+    @validator("key", "name", "key_type", "enable")
+    def name_not_empty(cls, v):
+        if isinstance(v, str) and len(v.strip()) == 0:
+            raise ParamsError('不能为空')
+        if not v:
+            raise ParamsError("不能为空")
+        return v
